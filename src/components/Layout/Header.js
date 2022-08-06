a5c7b1596341afa45f588eb994836edfedbd6ea9
AiperiAkimova <aiperi.peaksoft@gmail.com>
2022-08-06 13:14:51 +0600

    completed whole page layout

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,12 @@
 import classes from './Header.module.css'  //classes kak object bolup ichinde svoistvoloru bar konkrettu, bir componentke ele tieshesi bar
 import meals from "../../assets/meals.jpeg";
+import { HeaderCartButton } from './HeaderCartButton';
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={meals} alt="food-img" />

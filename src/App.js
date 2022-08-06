0ae334a4d0b2b3380320b42d3fe73b4f90b4bf37
AiperiAkimova <aiperi.peaksoft@gmail.com>
2022-08-06 11:52:06 +0600

    first looking header layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
 import './App.css';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+   <Header />
   );
 }
 
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import classes from './Header.module.css'  //classes kak object bolup ichinde svoistvoloru bar konkrettu, bir componentke ele tieshesi bar
+import meals from "../../assets/meals.jpeg";
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={meals} alt="food-img" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
